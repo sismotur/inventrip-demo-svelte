08c182eb50e2794422d998c06ed1caeaec624ad0
felipesanti <fsanti@Felipes-MacBook-Pro.local>
2023-11-06 22:56:34 +0100

    Remove static rendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
-<h1 class="text-3xl font-bold">Hello world!</h1>
+<script>
+  let n = 1;
+</script>
+
+<h1 class="text-3xl font-bold">Hello world {n}!</h1>
+<button type="button" on:click={() => (n += 1)}>Inc N</button>
 
 <style lang="postcss">
   :global(html) {
