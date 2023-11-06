59cf9409c15a163306644106166de0f52b8d6613
felipesanti <fsanti@Felipes-MacBook-Pro.local>
2023-11-06 21:53:56 +0100

    Remove old routes - fix Tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+</script>
+  
+<nav>
+  <ul>
+    <li>Element 1</li>
+    <li>Element 2</li>
+  </ul>
+</nav>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Hello World</h1>
+<h1 class="text-3xl font-bold">Hello world!</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
+
+  
